73467d1f1cda515f33b7ea85bccfe76ce7ee1759
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-14 11:01:02 -0300

    Finalizando requisito 5 ao 8

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const form = getById('formulario');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const email = getById('input-email').value;
+  const emailFixed = getById('input-fixed-email').value;
   const password = getById('input-password').value;
-  if (email === 'tryber@teste.com' && password === '123456') {
+  if (emailFixed === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
